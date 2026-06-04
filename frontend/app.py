24392e9204daff3a4f301fb2020ae6efd8bb0e52
Suchitra-dev30 <ramagallasuchitra@gmail.com>
2026-06-04 18:19:48 +0530

    connect frontend to render backend

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://ai-pdf-chatbot-nmhw.onrender.com"
 
 st.set_page_config(
     page_title="AI PDF Chatbot",
